f0bab4de383e1635d80fd2efa83a3a73d0b215bf
timothyjamesmarias <timothyjamesmarias@gmail.com>
2026-01-07 21:44:55 -0800

    more stupid config fixes

---
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,5 @@
+-- Initial schema for Family Archive application
+-- This is a placeholder migration to allow the application to start
+-- Add your initial tables and schema here
+
+-- Example: You can add tables as you build your application
