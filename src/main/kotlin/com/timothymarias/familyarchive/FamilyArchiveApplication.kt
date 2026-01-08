7e1bb94cc37e472d488c71c120040f6c7a4cf31c
timothyjamesmarias <timothyjamesmarias@gmail.com>
2026-01-07 20:50:14 -0800

    init commit

---
+++ src/main/kotlin/com/timothymarias/familyarchive/FamilyArchiveApplication.kt
@@ -0,0 +1,11 @@
+package com.timothymarias.familyarchive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FamilyArchiveApplication
+
+fun main(args: Array<String>) {
+    runApplication<FamilyArchiveApplication>(*args)
+}
---
+++ src/test/kotlin/com/timothymarias/familyarchive/FamilyArchiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.timothymarias.familyarchive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FamilyArchiveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
